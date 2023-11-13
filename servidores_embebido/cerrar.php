ef37392d518c5939ee4c5d1e5ecef5ab9c1f99bd
caro <csg0010@alu.medac.es>
2023-11-13 17:42:33 +0100

    final

--- servidores_embebido/cerrar.php
+++ servidores_embebido/cerrar.php
@@ -1,7 +1,7 @@
 <?php 
     session_start();
     if (isset($_SESSION["usuario"])) {
-       session_destroy();
+       session_destroy(); //para destruir la sesion una vez terminemos
     }
-    header('Location: embebidito.php');
+    header('Location: embebidito.php'); //y asi nos lleva el login
 ?>
