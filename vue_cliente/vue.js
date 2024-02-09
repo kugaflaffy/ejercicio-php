e92345ed7d37f5e2349f8239bebc92b8c21e6a57
caro <csg0010@alu.medac.es>
2024-02-09 17:57:54 +0100

    hoy

--- vue_cliente/vue.js
+++ vue_cliente/vue.js
@@ -2,10 +2,67 @@ var app=new Vue ({
     el: '#nombrecito',
     data:{
         animal : ''
-    }
-
-   
+    } 
 })
+
+var app=new Vue({
+el: '#muchosif',
+data:{ 
+    numero : -10
+}
+})
+
+
+var app=new Vue({
+el: '#unfor',
+data:{ 
+    personajes: ['my melody', 'kuromi', 'cinna', 'dani', 'keropi', 'pochacco']
+}
+})
+
+var app=new Vue({
+    el: '#on',
+    data:{ 
+        contadorcito: 0
+    },
+         
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      mensaje: '',
+      mensajeEnviado: ''
+    },
+    methods: {
+      enviarFormulario: function() {
+        this.mensajeEnviado = 'Mensaje enviado: ' + this.mensaje;
+        this.mensaje = ''; // Limpiar el campo de entrada después de enviar el mensaje
+      }
+    }
+  });
+
+  var app=new Vue({
+    el: '#css',
+    data: { 
+      tamañoFuente: 20,
+      colorParrafo: '#000'
+    }
+})
+
+    var app=new Vue({
+        el: '#vbind',
+        data:{ 
+            actividad: '',
+            actividades: []
+        },
+        methods: {
+            actualizarLista: function() {
+                this.actividades.push(this.actividad);
+                this.actividad = '';
+            }
+        }
+        })
 
 
 
